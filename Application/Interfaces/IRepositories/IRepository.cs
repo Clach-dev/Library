c3e6fab0b785262766d2d5205ab9044be521ecba
Clach-dev <yuhkno.siarhei@gmail.com>
2024-11-19 21:53:33 +0300

    Add repository interfaces

---
+++ Application/Interfaces/IRepositories/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq.Expressions;
+
+namespace Application.Interfaces.IRepositories;
+
+public interface IRepository<TEntity> where TEntity : class
+{
+    Task<IEnumerable<TEntity>> GetAllAsync(CancellationToken cancellationToken);
+    
+    Task<TEntity> GetByIdAsync(int id, CancellationToken cancellationToken);
+    
+    Task<TEntity> GetByPredicateAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken);
+    
+    Task<TEntity> AddAsync(TEntity entity, CancellationToken cancellationToken);
+    
+    Task<TEntity> UpdateAsync(TEntity entity, CancellationToken cancellationToken);
+    
+    Task DeleteAsync(TEntity entity, CancellationToken cancellationToken);
+}
