82f10b18844504709703d2170dd31bca2698468d
Clach-dev <yuhkno.siarhei@gmail.com>
2024-11-21 01:04:04 +0300

    new implementation for unit of work

--- Application/Interfaces/IRepositories/IUnitOfWork.cs
+++ Application/Interfaces/IRepositories/IUnitOfWork.cs
@@ -2,6 +2,5 @@
 
 public interface IUnitOfWork : IDisposable
 {
-    IRepository<TEntity> GetRepository<TEntity>() where TEntity : class;
     Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
 }
--- Infrastructure/Data/Repositories/UnitOfWork.cs
+++ Infrastructure/Data/Repositories/UnitOfWork.cs
@@ -1,36 +1,35 @@
 ﻿using Application.Interfaces.IRepositories;
-using Domain.Entities;
 
 namespace Infrastructure.Data.Repositories;
 
 public class UnitOfWork : IUnitOfWork
 {
     private readonly LibraryDbContext _context;
-    private readonly Dictionary<Type, Func<LibraryDbContext, object>> _repositoryFactories;
+    
+    private IAuthorRepository _authorRepository;
+    
+    private IBookRepository _bookRepository;
+    
+    private IGenreRepository _genreRepository;
+    
+    private IReservationRepository _reservationRepository;
+    
+    private IUserRepository _userRepository;
 
     public UnitOfWork(LibraryDbContext context)
     {
         _context = context;
-        _repositoryFactories = new Dictionary<Type, Func<LibraryDbContext, object>>
-        {
-            { typeof(Author), ctx => new AuthorRepository(ctx) },
-            { typeof(Book), ctx => new BookRepository(ctx) },
-            { typeof(Genre), ctx => new GenreRepository(ctx) },
-            { typeof(Reservation), ctx => new ReservationRepository(ctx) },
-            { typeof(User), ctx => new UserRepository(ctx) },
-        };
     }
 
-    public IRepository<TEntity> GetRepository<TEntity>() where TEntity : class
-    {
-        if (_repositoryFactories.TryGetValue(typeof(TEntity), out var factory))
-        {
-            var repository = (IRepository<TEntity>)factory.Invoke(_context);
-            return repository;
-        }
-        else
-            throw new InvalidOperationException($"No repository factory found for type {typeof(TEntity).Name}.");
-    }
+    public IAuthorRepository Authors => _authorRepository ??= new AuthorRepository(_context);
+
+    public IBookRepository Books => _bookRepository ??= new BookRepository(_context);
+    
+    public IGenreRepository Genres => _genreRepository ??= new GenreRepository(_context);
+    
+    public IReservationRepository Reservations  => _reservationRepository ??= new ReservationRepository(_context);
+    
+    public IUserRepository Users => _userRepository ??= new UserRepository(_context);
 
     public async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
     {
