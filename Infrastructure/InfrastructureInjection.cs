e3ac99cd38bb2adccab4e3882afe497f593a20ca
Clach-dev <yuhkno.siarhei@gmail.com>
2024-11-25 20:14:31 +0300

    Inject Infrastructure

--- Infrastructure/InfrastructureInjection.cs
+++ Infrastructure/InfrastructureInjection.cs
@@ -1,5 +1,38 @@
-﻿namespace Infrastructure;
+﻿using Application.Interfaces.IRepositories;
+using Infrastructure.Data;
+using Infrastructure.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
-public class InfrastructureInjection
+namespace Infrastructure;
+
+public static class InfrastructureInjection
 {
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        return services
+            .AddDatabase(configuration)
+            .AddRepositories();
+    }
+    
+    private static IServiceCollection AddDatabase(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("SqlConnectionString");
+        return services.AddDbContext<LibraryDbContext>(options =>
+            options
+                .UseSqlServer(connectionString)
+                .UseLazyLoadingProxies());
+    }
+
+    private static IServiceCollection AddRepositories(this IServiceCollection services)
+    {
+        return services
+            .AddScoped<IAuthorRepository, AuthorRepository>()
+            .AddScoped<IBookRepository, BookRepository>()
+            .AddScoped<IGenreRepository, GenreRepository>()
+            .AddScoped<IRefreshTokenRepository, RefreshTokenRepository>()
+            .AddScoped<IReservationRepository, ReservationRepository>()
+            .AddScoped<IUserRepository, UserRepository>();
+    }
 }
