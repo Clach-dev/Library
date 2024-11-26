c0bb65d0312a427589839645fbfe845d3c6c6c56
Clach-dev <yuhkno.siarhei@gmail.com>
2024-11-26 19:25:06 +0300

    fix token generator

---
+++ Application/Algorithms/TokensGenerator.cs
@@ -0,0 +1,48 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Domain.Entities;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Application.Algorithms;
+
+public class TokensGenerator(IConfiguration configuration)
+{
+    public string GenerateAccessToken(User user, IEnumerable<string> userRoles)
+    {
+        var claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.NameIdentifier, user.Id.ToString())
+        };
+
+        foreach (var role in userRoles)
+        {
+            claims.Add(new Claim(ClaimTypes.Role, role));
+        }
+
+        var tokenExpires = DateTime.UtcNow.AddMinutes(GetJwtSetting<double>("AccessTokenExpiresInMinutes"));
+
+        var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(GetJwtSetting<string>("Key")));
+        var signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha512Signature);
+        var securityToken = new JwtSecurityToken(
+            claims: claims,
+            expires: tokenExpires,
+            issuer: GetJwtSetting<string>("Issuer"),
+            signingCredentials: signingCredentials);
+
+        return new JwtSecurityTokenHandler().WriteToken(securityToken);
+    }
+
+    public RefreshToken GenerateRefreshToken(User user)
+    {
+        return new RefreshToken()
+        {
+            Token = Guid.NewGuid(),
+            ExpirationDate = DateTime.UtcNow.AddMinutes(GetJwtSetting<double>("RefreshTokenExpiresInMinutes")),
+            User = user
+        };
+    }
+
+    private T GetJwtSetting<T>(string key) => configuration.GetValue<T>($"JwtSettings:{key}");
+}
