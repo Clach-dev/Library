cf15502bbcc55abe3d59aceeb12763ba0f93902e
Clach-dev <yuhkno.siarhei@gmail.com>
2024-11-30 01:53:29 +0300

    Add Result pattern

---
+++ Application/Utils/Result.cs
@@ -0,0 +1,5 @@
+﻿using System.Net;
+
+namespace Application.Utils;
+
+public record Result<T>(bool IsSuccess, HttpStatusCode StatusCode, T? Value, string[] Errors);
---
+++ Application/Utils/ResultBuilder.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+
+namespace Application.Utils;
+
+public static class ResultBuilder 
+{
+    public static Result<T> CreatedResult<T>(T data) => new (true, HttpStatusCode.Created, data, [string.Empty]);
+    
+    public static Result<T> SuccessResult<T>(T data) => new (true, HttpStatusCode.OK, data, [string.Empty]);
+    
+    public static Result<T> NoContentResult<T>() => new (true, HttpStatusCode.NoContent, default, [string.Empty]);
+    
+    public static Result<T> UnauthorizedResult<T>(string message) => new (false, HttpStatusCode.Unauthorized, default, [message]);
+    
+    public static Result<T> NotFoundResult<T>(string message) => new (false, HttpStatusCode.NotFound, default, [message]);
+}
