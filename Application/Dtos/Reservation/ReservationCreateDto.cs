8c363207c451dc065f6f169f79f75c4f853a0e7e
Clach-dev <yuhkno.siarhei@gmail.com>
2024-12-01 02:37:17 +0300

    Reservation DTOs fix

--- Application/Dtos/Reservation/ReservationCreateDto.cs
+++ Application/Dtos/Reservation/ReservationCreateDto.cs
@@ -4,9 +4,5 @@
 /// DTO for Reservation Create operation
 /// </summary>
 /// <param name="BookId">Guid identifier of Book</param>
-/// <param name="ReceiptDate">DateTime which represents date and time of book receiving</param>
-/// <param name="ReturnDate">DateTime which represents date and time when the book need be returned</param>
 public record ReservationCreateDto(
-    Guid BookId,
-    DateTime ReceiptDate,
-    DateTime ReturnDate);
+    Guid BookId);
--- Application/Dtos/Reservation/ReservationReducedReadDto.cs
+++ Application/Dtos/Reservation/ReservationReducedReadDto.cs
@@ -1,19 +1,16 @@
 ﻿using Application.Dtos.Book;
-using Application.Dtos.User;
 
 namespace Application.Dtos.Reservation;
 
 /// <summary>
-/// DTO for Reservation Read operation
+/// Dto for Reservation reduced Read operation
 /// </summary>
 /// <param name="Id">Guid identifier of Reservation</param>
 /// <param name="ReceiptDate">DateTime which represents date and time of book receiving</param>
 /// <param name="ReturnDate">DateTime which represents date and time of book returning</param>
 /// <param name="Book">BookReducedReadDto which contains Book</param>
-/// <param name="User">UserReducedReadDto which contains User</param>
-public record ReservationReadDto(
+public record ReservationReducedReadDto(
     Guid Id,
     DateTime ReceiptDate,
     DateTime ReturnDate,
-    BookReducedReadDto Book,
-    UserReducedReadDto User);
+    BookReducedReadDto Book);
