bfb3a0df98855cbcd23317b973ed3301ef3125c8
Clach-dev <yuhkno.siarhei@gmail.com>
2024-12-01 14:41:09 +0300

    reservation dto fix

--- Application/Dtos/Reservation/ReservationUpdateDto.cs
+++ Application/Dtos/Reservation/ReservationUpdateDto.cs
@@ -4,9 +4,14 @@
 /// DTO for Reservation Update operation
 /// </summary>
 /// <param name="Id">Guid identifier of Reservation</param>
+/// <param name="BookId">Guid identifier of Book</param>
+/// <param name="UserId">Guid identifier of User</param>
 /// <param name="ReturnDate">DateTime which represents date and time when the book need be returned</param>
 /// <param name="IsReturned">bool which represents if book is returned</param>
 public record ReservationUpdateDto(
     Guid Id,
+    Guid? BookId,
+    Guid? UserId,
+    DateTime? ReceiptDate,
     DateTime? ReturnDate,
     bool? IsReturned);
