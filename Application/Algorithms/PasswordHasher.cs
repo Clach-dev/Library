757116fbcd6d9be8c16d41df76767059b5a624b3
Clach-dev <yuhkno.siarhei@gmail.com>
2024-12-02 00:44:31 +0300

    passwordd hasher fix

--- Application/Algorithms/PasswordHasher.cs
+++ Application/Algorithms/PasswordHasher.cs
@@ -2,6 +2,7 @@
 using System.Text;
 using Application.Interfaces.IAlgorithms;
 using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Application.Algorithms;
 
@@ -16,7 +17,6 @@ public class PasswordHasher : IPasswordHasher
         _key = Encoding.UTF8.GetBytes(key);
     }
 
-    [Obsolete("Obsolete")]
     public string HashPassword(string password)
     {
         if (password == null)
@@ -26,16 +26,16 @@ public class PasswordHasher : IPasswordHasher
 
         using (var hmac = new HMACSHA256(_key))
         {
-            byte[] salt = GenerateSalt(16);
-            byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
-            byte[] saltedPassword = new byte[salt.Length + passwordBytes.Length];
+            var salt = GenerateSalt(16);
+            var passwordBytes = Encoding.UTF8.GetBytes(password);
+            var saltedPassword = new byte[salt.Length + passwordBytes.Length];
 
             Buffer.BlockCopy(salt, 0, saltedPassword, 0, salt.Length);
             Buffer.BlockCopy(passwordBytes, 0, saltedPassword, salt.Length, passwordBytes.Length);
 
-            byte[] hash = hmac.ComputeHash(saltedPassword);
+            var hash = hmac.ComputeHash(saltedPassword);
 
-            byte[] result = new byte[salt.Length + hash.Length];
+            var result = new byte[salt.Length + hash.Length];
             Buffer.BlockCopy(salt, 0, result, 0, salt.Length);
             Buffer.BlockCopy(hash, 0, result, salt.Length, hash.Length);
 
@@ -54,33 +54,32 @@ public class PasswordHasher : IPasswordHasher
             throw new ArgumentNullException(nameof(password), "Password cannot be null!");
         }
 
-        byte[] hashBytes = Convert.FromBase64String(hashedPassword);
+        var hashBytes = Convert.FromBase64String(hashedPassword);
 
-        byte[] salt = new byte[16];
+        var salt = new byte[16];
         Buffer.BlockCopy(hashBytes, 0, salt, 0, salt.Length);
 
-        byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
-        byte[] saltedPassword = new byte[salt.Length + passwordBytes.Length];
+        var passwordBytes = Encoding.UTF8.GetBytes(password);
+        var saltedPassword = new byte[salt.Length + passwordBytes.Length];
 
         Buffer.BlockCopy(salt, 0, saltedPassword, 0, salt.Length);
         Buffer.BlockCopy(passwordBytes, 0, saltedPassword, salt.Length, passwordBytes.Length);
 
         using (var hmac = new HMACSHA256(_key))
         {
-            byte[] computedHash = hmac.ComputeHash(saltedPassword);
+            var computedHash = hmac.ComputeHash(saltedPassword);
 
-            byte[] storedHash = new byte[computedHash.Length];
+            var storedHash = new byte[computedHash.Length];
             Buffer.BlockCopy(hashBytes, salt.Length, storedHash, 0, storedHash.Length);
 
             return ByteArraysEqual(computedHash, storedHash);
         }
     }
 
-    [Obsolete("Obsolete")]
     private static byte[] GenerateSalt(int length)
     {
-        byte[] salt = new byte[length];
-        using (var rng = new RNGCryptoServiceProvider())
+        var salt = new byte[length];
+        using (var rng = RandomNumberGenerator.Create())
         {
             rng.GetBytes(salt);
         }
@@ -90,13 +89,9 @@ public class PasswordHasher : IPasswordHasher
     private static bool ByteArraysEqual(byte[] b1, byte[] b2)
     {
         if (b1 == b2) return true;
-        if (b1 == null || b2 == null) return false;
+        if (b1.IsNullOrEmpty() || b2.IsNullOrEmpty()) return false;
         if (b1.Length != b2.Length) return false;
 
-        for (int i = 0; i < b1.Length; i++)
-        {
-            if (b1[i] != b2[i]) return false;
-        }
-        return true;
+        return !b1.Where((t, i) => t != b2[i]).Any();
     }
 }
